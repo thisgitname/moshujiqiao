895655b2d5111c08be4ed9543db3b2652bacf1ac
这是gitname <2806348881@qq.com>
2025-06-20 17:22:48 +0800

    6

--- src/app/(hero)/scale/page.tsx
+++ src/app/(hero)/scale/page.tsx
@@ -1,6 +1,5 @@
 import React from 'react'
 import Hero from '../../components/hero'
-import ScaleSrc from  '@/public/4.png'
 import { Metadata } from 'next'
 
 export const metadata:Metadata = {
@@ -8,6 +7,6 @@ export const metadata:Metadata = {
 }
 export default function page() {
   return (
-    <Hero imgUrl={ScaleSrc} altTxt="Scale" content="这是我这个Scale页面的内容"></Hero>
+    <Hero imgUrl="/4.png" altTxt="Scale" content="这是我这个Scale页面的内容"></Hero>
  )
 }
